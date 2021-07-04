0dbf47c1814782b28ae70c5b6fc6283b22d703bf
Filip <szymanski.fp@gmail.com>
2021-07-04 23:23:35 +0200

    finished main file

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Otomanopee+One&display=swap" rel="stylesheet">
+    <title>Todo App</title>
+</head>
+
+
+<body>
+    <div class="form" action="addTodo.php">
+        <form action="">
+            <label for="creator">Creator: </label><br>
+            <input type="text" placeholder="Ex. David"><br>
+
+            <label for="todo">Todo: </label><br>
+            <textarea name="todo" id="todo"></textarea><br>
+
+            <button>SUBMIT</button>
+        </form>
+    </div>
+</body>
+
+</html>
